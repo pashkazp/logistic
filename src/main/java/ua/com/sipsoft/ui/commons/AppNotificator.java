912c13a559d8d631a256119a3c1786d45a6cf601
DePavlo <pashka@ukr.net>
2019-11-15 22:08:30 +0200

    Added color to AppNotificator

--- src/main/java/ua/com/sipsoft/ui/commons/AppNotificator.java
+++ src/main/java/ua/com/sipsoft/ui/commons/AppNotificator.java
@@ -5,6 +5,7 @@ import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.Notification.Position;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
 import lombok.extern.slf4j.Slf4j;
@@ -39,14 +40,42 @@ public class AppNotificator {
     public static <T extends Object> void notify(int duration, String... note) {
 	if (note != null && duration >= 0) {
 	    Class<?> callerClass = walker.getCallerClass();
-	    for (String string : note) {
-		Notification n = new Notification(new HorizontalLayout(new Span(string)));
-		n.setDuration(duration);
-		n.setPosition(Position.BOTTOM_STRETCH);
+	    doNotify(duration, callerClass, note);
+	}
+    }
 
-		log.info(callerClass.getName() + ": " + string);
-		n.open();
-	    }
+    public static <T extends Object> void notify(int duration, NotificationVariant variant, String... note) {
+	if (note != null && duration >= 0) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(duration, callerClass, variant, note);
+	}
+    }
+
+    public static <T extends Object> void notifyPrimary(int duration, String... note) {
+	if (note != null && duration >= 0) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(duration, callerClass, NotificationVariant.LUMO_PRIMARY, note);
+	}
+    }
+
+    public static <T extends Object> void notifyContrast(int duration, String... note) {
+	if (note != null && duration >= 0) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(duration, callerClass, NotificationVariant.LUMO_CONTRAST, note);
+	}
+    }
+
+    public static <T extends Object> void notifySuccess(int duration, String... note) {
+	if (note != null && duration >= 0) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(duration, callerClass, NotificationVariant.LUMO_SUCCESS, note);
+	}
+    }
+
+    public static <T extends Object> void notifyError(int duration, String... note) {
+	if (note != null && duration >= 0) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(duration, callerClass, NotificationVariant.LUMO_ERROR, note);
 	}
     }
 
@@ -59,13 +88,66 @@ public class AppNotificator {
     public static <T extends Object> void notify(String... note) {
 	if (note != null) {
 	    Class<?> callerClass = walker.getCallerClass();
-	    for (String string : note) {
-		Notification n = new Notification(new HorizontalLayout(new Span(string)));
-		n.setDuration(3000);
-		n.setPosition(Position.BOTTOM_STRETCH);
-		log.info(callerClass.getName() + ": " + string);
-		n.open();
-	    }
+	    doNotify(3000, callerClass, note);
 	}
     }
+
+    public static <T extends Object> void notify(NotificationVariant variant, String... note) {
+	if (note != null) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(3000, callerClass, variant, note);
+	}
+    }
+
+    public static <T extends Object> void notifyPrimary(String... note) {
+	if (note != null) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(3000, callerClass, NotificationVariant.LUMO_PRIMARY, note);
+	}
+    }
+
+    public static <T extends Object> void notifyContrast(String... note) {
+	if (note != null) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(3000, callerClass, NotificationVariant.LUMO_CONTRAST, note);
+	}
+    }
+
+    public static <T extends Object> void notifySuccess(String... note) {
+	if (note != null) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(3000, callerClass, NotificationVariant.LUMO_SUCCESS, note);
+	}
+    }
+
+    public static <T extends Object> void notifyError(String... note) {
+	if (note != null) {
+	    Class<?> callerClass = walker.getCallerClass();
+	    doNotify(3000, callerClass, NotificationVariant.LUMO_ERROR, note);
+	}
+    }
+
+    private static void doNotify(int duration, Class<?> callerClass, NotificationVariant variant, String... note) {
+	for (String string : note) {
+	    Notification n = new Notification(new HorizontalLayout(new Span(string)));
+	    n.addThemeVariants(variant);
+	    openNotify(duration, callerClass, string, n);
+	}
+    }
+
+    private static void doNotify(int duration, Class<?> callerClass, String... note) {
+	for (String string : note) {
+	    Notification n = new Notification(new HorizontalLayout(new Span(string)));
+	    openNotify(duration, callerClass, string, n);
+	}
+    }
+
+    private static void openNotify(int duration, Class<?> callerClass, String string, Notification notiffcation) {
+	notiffcation.setDuration(duration);
+	notiffcation.setPosition(Position.BOTTOM_STRETCH);
+
+	log.info(callerClass.getName() + ": " + string);
+	notiffcation.open();
+    }
+
 }
